f2cf393b84679b436321881e4bcaee819031ad87
Diozafira <149204707+Diozafira@users.noreply.github.com>
2025-05-24 11:17:26 +0300

    Migration to Booking Model

---
+++ Migrations/20250524081312_ModelBookingChanged.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel_Philoxenia.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModelBookingChanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Capacity",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "Bookings");
+
+            migrationBuilder.RenameColumn(
+                name: "CheckOut",
+                table: "Bookings",
+                newName: "ReservationDateTo");
+
+            migrationBuilder.RenameColumn(
+                name: "CheckIn",
+                table: "Bookings",
+                newName: "ReservationDateFrom");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PricePerNight",
+                table: "Rooms",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Canceled",
+                table: "Bookings",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckInDate",
+                table: "Bookings",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckOutDate",
+                table: "Bookings",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<double>(
+                name: "ReservationDayPrice",
+                table: "Bookings",
+                type: "double",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Canceled",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "CheckInDate",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "CheckOutDate",
+                table: "Bookings");
+
+            migrationBuilder.DropColumn(
+                name: "ReservationDayPrice",
+                table: "Bookings");
+
+            migrationBuilder.RenameColumn(
+                name: "ReservationDateTo",
+                table: "Bookings",
+                newName: "CheckOut");
+
+            migrationBuilder.RenameColumn(
+                name: "ReservationDateFrom",
+                table: "Bookings",
+                newName: "CheckIn");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PricePerNight",
+                table: "Rooms",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Capacity",
+                table: "Rooms",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalPrice",
+                table: "Bookings",
+                type: "decimal(65,30)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
--- Models/Booking.cs
+++ Models/Booking.cs
@@ -8,15 +8,19 @@ namespace Hotel_Philoxenia.Models
 {
     public class Booking
     {
-        public int Id { get; set; }
-        public int RoomId { get; set; }
-        public int CustomerId { get; set; }
-        public DateTime CheckIn { get; set; }
-        public DateTime CheckOut { get; set; }
-        public decimal TotalPrice { get; set; }
 
-        public Room Room { get; set; }
-        public Customer Customer { get; set; }
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime ReservationDateFrom { get; set; }
+        public DateTime ReservationDateTo { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public bool Canceled { get; set; }
+        public double ReservationDayPrice { get; set; }
+        public Room Room { get; set; } = new();
+        public Customer Customer { get; set; } = new();
     }
 
 }
