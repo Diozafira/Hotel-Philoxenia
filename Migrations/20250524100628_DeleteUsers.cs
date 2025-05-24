8396368f4fbe6d8f7725b6da88a940e6b00721bd
Diozafira <149204707+Diozafira@users.noreply.github.com>
2025-05-24 13:12:46 +0300

    Migration DeleteUsers form Database added

---
+++ Migrations/20250524100628_DeleteUsers.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hotel_Philoxenia.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteUsers : Migration
+    {
+        
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Users WHERE Id >= 1 AND Id <= 5;");
+        }
+    }
+}
