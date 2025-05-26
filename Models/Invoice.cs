d71726e0542513eb09fa639849bb775638ecc202
Diozafira <149204707+Diozafira@users.noreply.github.com>
2025-05-26 21:48:12 +0300

    Create Invoice Model

---
+++ Models/Invoice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Philoxenia.Models
+{
+    internal class Invoice
+    {
+        public class InvoiceModel
+        {
+            public int InvoiceId { get; set; }
+            public string CustomerFullName { get; set; }
+            public string CustomerAFM { get; set; }
+            public int BookingId { get; set; }
+            public double TotalCost { get; set; }
+        }
+
+    }
+}
